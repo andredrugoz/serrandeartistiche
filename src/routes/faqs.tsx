a9e0d88680a708db297c540c27669f9ac01dc6b9
andredrugoz <andrea.nardi@checkupdigitale.com>
2023-07-06 22:51:09 +0200

    commit

--- src/routes/faqs.tsx
+++ src/routes/faqs.tsx
@@ -34,6 +34,27 @@ export default function FaqsPage() {
             Realizziamo e curiamo nei minimi dettagli l’opera assicurandoti un risultato ottimale. Garantiamo la durata evitandoti nuovi interventi che ti comporterebbero spese inutili. 
             </p>
           </details>
+
+          <details className="mt-4 block rounded-sm border border-gray-200 px-4 hover:border-primary-300">
+            <summary className="-mx-4 cursor-pointer py-3 px-4">
+            Quali sono i benefici?
+            </summary>
+            <p>
+            <b>Vivibilità</b><br/>
+            l’Opera sulla tua serranda contribuirà a migliorare e rendere più vivibile e piacevole il tuo quartiere. Gli abitanti te lo faranno notare e ti ringrazieranno. 
+            </p>
+
+            <p><b>Cultura</b><br/>
+            Promuoverai grazie al tuo punto vendita l’arte visiva diventando un punto di attrattivo anche per il pubblico appassionato di street art. 
+            </p>
+            <p><b>Pubblicità</b><br/>
+            I tuoi clienti saranno ancora più invogliati a visitare il negozio e ne faranno passaparola. Il tuo brand crescerà aumentato il valore percepito dal cliente 
+            </p>
+            <p><b>Clienti</b><br/>
+            Riceverai della pubblicità inaspettata e che non dovrai pagare perchè i passanti saranno attratti dal fotografare e condividere l’opera che verrà realizzata
+            </p>
+          </details>
+          
         </ArticleContent>
 
         <ArticleMedia>
