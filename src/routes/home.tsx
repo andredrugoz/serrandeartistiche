9bad1226440a2c53b38c21683ff1b38cc8a205e8
andredrugoz <andrea.nardi@checkupdigitale.com>
2023-07-07 02:55:27 +0200

    link

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,7 @@
 import { Hero } from '@/components/hero'
 import { HeroIllustration } from '@/components/hero-illustration'
 import { Layout } from '@/components/layout'
+import immagine from "../images/358535129_132655923189230_5444935758060835910_n.jpg"
 
 export default function HomePage() {
   return (
